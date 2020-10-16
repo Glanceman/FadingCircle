5a494afb208b1abf48fecd06125c62ed7bdc9110
Glanceman <72264741+Glanceman@users.noreply.github.com>
2020-10-16 16:15:44 +0800

    enable sound function

--- sketch.js
+++ sketch.js
@@ -8,23 +8,23 @@ let song;
 let amplitude;
 
 function setup() {
-  //song=loadSound('/01.mp3');
+  song=loadSound('/01.mp3');
   createCanvas(windowWidth, windowHeight);
   disappear=255;
-  //amplitude=new p5.Amplitude();
+  amplitude=new p5.Amplitude();
 }
 
 function draw() {
   background(bR,bG,bB,20);
   if(disappear>0){
     display("Designed by Ben",windowWidth/2,windowHeight/2,disappear,disappear);
-    //display("BGM:今日のことは忘れられないよ ",windowWidth/2,windowHeight/2+50,disappear,disappear);
+    display("BGM:今日のことは忘れられないよ ",windowWidth/2,windowHeight/2+50,disappear,disappear);
    disappear--; 
   }else{
-    // if (song.isPlaying()==false){
-    //   song.play();
-    // }
-    // let level= amplitude.getLevel()*10;
+     if (song.isPlaying()==false){
+      song.play();
+     }
+     let level= amplitude.getLevel()*10;
   FadingEillipse(windowWidth/2,windowHeight/2,windowHeight*3/4,size,150,220,255,bR,bG,bB,10);
   FadingEillipse(windowWidth/4,windowHeight/4,windowHeight*3/4,size,200,220,255,bR,bG,bB,10);
   FadingEillipse(windowWidth/4,windowHeight*3/4,windowHeight*3/4,size,200,80,150,bR,bG,bB,10);
